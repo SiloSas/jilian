1115260f35c20325761a21d2529fee034a2c926c
Loann <loann.delgado@gmail.com>
2021-04-14 16:30:45 +0200

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,12 +98,12 @@ module.exports = {
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     //trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `G-CTV5502SQR`,
+      },
+    },
     // `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
